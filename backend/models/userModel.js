e656f27ea983a052a10d7027c56ec689e585be1d
Hamza <>
2023-09-05 15:51:11 +0500

    added feat login & signup

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
@@ -17,6 +18,11 @@ const userSchema = new Schema({
 
 // static signup method
 userSchema.statics.signup = async function (email, password) {
+  if (!email || !password) throw new Error("All fields must be filled");
+  if (!validator.isEmail(email)) throw new Error("Not a valid email");
+  if (!validator.isStrongPassword(password))
+    throw new Error("Not a strong password");
+
   const exists = await this.findOne({ email });
 
   if (exists) {
@@ -27,6 +33,23 @@ userSchema.statics.signup = async function (email, password) {
   const hash = await bcrypt.hash(password, salt);
 
   const user = await this.create({ email, password: hash });
+
+  return user;
+};
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) throw new Error("All fields must be filled");
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw new Error("User not found.");
+  }
+
+  const match = bcrypt.compare(password, user.password);
+
+  if (!match) throw new Error("Invalid password");
 
   return user;
 };
